bb956429044333b33cc85861872198ece59e9e56
funnythingz <funnythingz69+github@gmail.com>
2015-03-06 00:13:06 +0900

    try

---
+++ test.js
@@ -0,0 +1,4 @@
+riot.tag('test', '<h3>{ test }</h3>', function(opts) {var test = 'TypeScript';
+this.test = test;
+
+});
---
+++ test.tag
@@ -0,0 +1,8 @@
+<test>
+
+  <h3>{ test }</h3>
+
+  var test: string = 'TypeScript'
+  this.test = test
+
+</test>
